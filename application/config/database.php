8b000d98028bc96c47ccca4374359f475af180bf
Jonah Khandros <Jonah@Jonahs-MacBook-Pro.local>
2016-03-24 12:52:38 -0700

    Hey there!

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost:8889',
+	'hostname' => 'localhost:3306',
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'lampbeltexam',
